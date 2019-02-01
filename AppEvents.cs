71fceb3687ea2cc23000bb1ed5c8d846b54ab668
Vitaclick <vitaliy.zema@gmail.com>
2019-02-01 19:50:16 +0300

    Initial commit

---
+++ AppEvents.cs
@@ -0,0 +1,214 @@
+#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using ProgressChangedEventArgs = Autodesk.Revit.DB.Events.ProgressChangedEventArgs;
+using TaskDialog = Autodesk.Revit.UI.TaskDialog;
+using TaskDialogResult = Autodesk.Revit.UI.TaskDialogResult;
+
+#endregion
+
+namespace RevitUpdater
+{
+  public static class Globals
+  {
+    public static string data;
+  }
+  class AppEvents : IExternalApplication
+  {
+    public List<ModelPath> data;
+
+    public Result OnStartup(UIControlledApplication a)
+    {
+
+      // Register related events
+      try
+      {
+        // register events
+        a.ControlledApplication.ProgressChanged += app_ClosingLinks;
+        a.ControlledApplication.DocumentOpening += app_DocumentOpening;
+        a.DialogBoxShowing += new EventHandler<Autodesk.Revit.UI.Events.DialogBoxShowingEventArgs>(AppDialogShowing);
+//        a.ControlledApplication.FailuresProcessing += new EventHandler<FailuresProcessingEventArgs>(OnFailuresProcessing);
+//        a.ControlledApplication.FailuresProcessing += new EventHandler<FailuresProcessingEventArgs>(CheckWarnings);
+      }
+      catch (Exception) { return Result.Failed;}
+
+      return Result.Succeeded;
+    }
+    
+    public Result OnShutdown(UIControlledApplication a)
+    {
+      a.ControlledApplication.ProgressChanged -= app_ClosingLinks;
+      a.ControlledApplication.DocumentOpening -= app_DocumentOpening;
+      a.DialogBoxShowing -= new EventHandler<Autodesk.Revit.UI.Events.DialogBoxShowingEventArgs>(AppDialogShowing);
+//      a.ControlledApplication.FailuresProcessing -= new EventHandler<FailuresProcessingEventArgs>(OnFailuresProcessing);
+//      a.ControlledApplication.FailuresProcessing -= new EventHandler<FailuresProcessingEventArgs>(CheckWarnings);
+      return Result.Succeeded;
+    }
+
+    private void CheckWarnings(object sender, FailuresProcessingEventArgs e)
+    {
+      FailuresAccessor fa = e.GetFailuresAccessor();
+
+      IList<FailureMessageAccessor> failList = fa.GetFailureMessages(); // Inside event handler, get all warnings
+
+      foreach (FailureMessageAccessor failure in failList)
+      {
+        // check FailureDefinitionIds against ones that you want to dismiss, 
+        FailureDefinitionId failID = failure.GetFailureDefinitionId();
+        var failureSeverity = fa.GetSeverity();
+        if (failureSeverity == FailureSeverity.Warning)
+        {
+          fa.DeleteWarning(failure);
+        }
+//        // prevent Revit from showing Unenclosed room warnings
+//        if (failID == BuiltInFailures.RoomFailures.RoomNotEnclosed)
+//        {
+//          fa.DeleteWarning(failure);
+//        }
+      }
+    }
+
+    void AppDialogShowing(object sender, DialogBoxShowingEventArgs args)
+    {
+      // DialogBoxShowingEventArgs has two subclasses - TaskDialogShowingEventArgs & MessageBoxShowingEventArgs
+      // In this case we are interested in this event if it is TaskDialog being shown. 
+      if (args is TaskDialogShowingEventArgs e)
+      {
+        //worry about this later - 1002 = cancel
+        if (e.DialogId == "TaskDialog_Unresolved_References") {
+          e.OverrideResult(1002);
+        }
+        //Don't sync newly created files. 1003 = close
+        if (e.DialogId == "TaskDialog_Local_Changes_Not_Synchronized_With_Central") {
+          e.OverrideResult(1003);
+        }
+        if (e.DialogId == "TaskDialog_Save_Changes_To_Local_File") {
+          //Relinquish unmodified elements and worksets
+          e.OverrideResult(1001);
+        }
+        if (e.DialogId == "TaskDialog_Copied_Central_Model")
+        {
+          args.OverrideResult((int)TaskDialogResult.Close);
+        }
+        if (e.Message == "The floor/roof overlaps the highlighted wall(s). Would you like to join geometry and cut the overlapping volume out of the wall(s)?")
+        {
+          // Call OverrideResult to cause the dialog to be dismissed with the specified return value
+          // (int) is used to convert the enum TaskDialogResult.No to its integer value which is the data type required by OverrideResult
+          e.OverrideResult((int)TaskDialogResult.No);
+        }
+      }
+
+//      // Get the help id of the showing dialog
+//      int dialogId = args.HelpId;
+//
+//      // Format the prompt information string
+//      String promptInfo = "A Revit dialog will be opened.\n";
+//      promptInfo += "The help id of this dialog is " + dialogId.ToString() + "\n";
+//      promptInfo += "If you don't want the dialog to open, please press cancel button";
+//
+//      // Show the prompt message, and allow the user to close the dialog directly.
+//      TaskDialog taskDialog = new TaskDialog("Revit");
+//      taskDialog.MainContent = promptInfo;
+//      TaskDialogCommonButtons buttons = TaskDialogCommonButtons.Ok |
+//                                        TaskDialogCommonButtons.Cancel;
+//      taskDialog.CommonButtons = buttons;
+//      TaskDialogResult result = taskDialog.Show();
+//      if (TaskDialogResult.Cancel == result)
+//      {
+//        // Do not show the Revit dialog
+//        args.OverrideResult(1);
+//      }
+//      else
+//      {
+//        // Continue to show the Revit dialog
+//        args.OverrideResult(0);
+//      }
+    }
+
+//    private void OnFailuresProcessing(object sender, FailuresProcessingEventArgs e)
+//    {
+//      FailuresAccessor failuresAccessor = e.GetFailuresAccessor();
+//      IEnumerable<FailureMessageAccessor> failureMessages = failuresAccessor.GetFailureMessages();
+//      foreach (FailureMessageAccessor failureMessage in failureMessages) {
+//        if (failureMessage.GetSeverity() == FailureSeverity.Warning) {
+//          failuresAccessor.DeleteWarning(failureMessage);
+//        }
+//      }
+//      e.SetProcessingResult(FailureProcessingResult.Continue);
+//    }
+
+    private void app_DocumentOpening(object sender, DocumentOpeningEventArgs e)
+    {
+//      try
+//      {
+//        var temp = new List<ModelPath>();
+//
+//        var modelPath = ModelPathUtils.ConvertUserVisiblePathToModelPath(@"RSN://" + e.PathName);
+
+
+//        var trans = new TransmissionData(TransmissionData.ReadTransmissionData(modelPath));
+//        foreach (var refId in trans.GetAllExternalFileReferenceIds())
+//        {
+//          var extRef = trans.GetLastSavedReferenceData(refId);
+//
+//          if (extRef.ExternalFileReferenceType == ExternalFileReferenceType.RevitLink)
+//          {
+//            var path = extRef.GetPath();
+//            temp.Add(path);
+//          }
+//        }
+        Globals.data = Path.GetFileNameWithoutExtension(e.PathName);
+
+//      }
+//        catch (Exception exception)
+//      {
+//        Console.WriteLine(exception);
+//        throw;
+//      }
+
+    }
+
+
+    public void app_ClosingLinks(object sender, ProgressChangedEventArgs e)
+    {
+      if (Globals.data != null)
+      {
+//        var openingLinksNames = new List<string>();
+//        foreach (ModelPath modelPath in Globals.data)
+//        {
+//          var filePath = ModelPathUtils.ConvertModelPathToUserVisiblePath(modelPath);
+//          openingLinksNames.Add(Path.GetFileNameWithoutExtension(filePath));
+//        }
+        // get document from event args
+//        if (openingLinksNames.Any(file => e.Caption.Contains(file)))
+        var state = e.Caption;
+
+        var file = Regex.Match(state, @"Загрузка \d\d\d\d|Loading \d\d\d\d");
+
+        if (file.Length > 0)
+        {
+          if ( !state.Contains(Globals.data))
+          {
+            if (e.Cancellable)
+            {
+              Debug.Write(state);
+              e.Cancel();
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ Updater.cs
@@ -0,0 +1,222 @@
+#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Application = Autodesk.Revit.ApplicationServices.Application;
+using TaskDialog = Autodesk.Revit.UI.TaskDialog;
+#endregion
+
+namespace RevitUpdater
+{
+  [Transaction(TransactionMode.Manual)]
+  public class Updater : IExternalCommand
+  {
+    public static class Globals
+    {
+      public static List<List<ModelPath>> data = new List<List<ModelPath>>(){};
+    }
+
+    private void app_ListOfLinks(ModelPath modelPath)
+    {
+      var temp = new List<ModelPath>();
+
+      var trans = new TransmissionData(TransmissionData.ReadTransmissionData(modelPath));
+      foreach (var refId in trans.GetAllExternalFileReferenceIds())
+      {
+        var extRef = trans.GetLastSavedReferenceData(refId);
+
+        if (extRef.ExternalFileReferenceType == ExternalFileReferenceType.RevitLink)
+        {
+          var path = extRef.GetPath();
+          temp.Add(path);
+        }
+      }
+
+      Globals.data.Add(temp);
+
+    }
+
+
+//    public void app_ClosingLinks(object sender, ProgressChangedEventArgs e)
+//    {
+//      if (RevitUpdater.Globals.data != null)
+//      {
+//        var openingLinksNames = new List<string>();
+//        foreach (ModelPath modelPath in RevitUpdater.Globals.data)
+//        {
+//          var filePath = ModelPathUtils.ConvertModelPathToUserVisiblePath(modelPath);
+//          openingLinksNames.Add(Path.GetFileNameWithoutExtension(filePath));
+//        }
+//        // get document from event args
+//        if (openingLinksNames.Any(file => e.Caption.Contains(file)) && e.LowerRange == e.Position)
+//        {
+//          if (e.Cancellable)
+//          {
+//            Debug.Write(e.Caption);
+//
+//            e.Cancel();
+//          }
+//        }
+//      }
+//    }
+
+
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            
+//            uiapp.Application.ProgressChanged += new EventHandler<ProgressChangedEventArgs>(app_ClosingLinks);
+//            uiapp.Application.DocumentOpening += new EventHandler<DocumentOpeningEventArgs>(app_DocumentOpening);
+
+
+            var modelPaths = GetModelPaths();
+            if (modelPaths == null) return Result.Cancelled;
+
+            // Set open configuration all-in
+            var openOpts = new OpenOptions();
+            openOpts.SetOpenWorksetsConfiguration(new WorksetConfiguration(WorksetConfigurationOption.OpenAllWorksets));
+
+          var preDocument = uidoc;
+
+          foreach (var modelPath in modelPaths)
+          {
+            app_ListOfLinks(modelPath);
+            // ver 2 opendactivedocument
+//            var openingUI = uiapp.OpenAndActivateDocument(modelPath, openOpts, false);
+//            var openedDoc = openingUI.Document;
+//
+//            if (preDocument != null)
+//            {
+//              preDocument = openingUI;
+//            }
+
+
+            // ver 1 opendocumentfile
+//            var openedDoc = app.OpenDocumentFile(modelPath, openOpts);
+//            var trans = new Transaction(openedDoc, "kek");
+//            trans.Start();
+//            openedDoc.Regenerate();
+//            trans.Commit();
+
+//            SyncWithCentral(openedDoc);
+//            openedDoc.Close(false);
+          }
+
+          //            CloseModelLinks(testModel);
+          // Open and sync model
+          //            var openedDoc = app.OpenDocumentFile(testModel, openOpts);
+
+//                uiapp.Application.ProgressChanged -= app_ClosingLinks;
+//      uiapp.Application.DocumentOpening -= app_DocumentOpening;
+
+            return Result.Succeeded;
+
+        }
+
+        private List<ModelPath> GetModelPaths()
+        {
+            // Collect folders to update
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (result.ToString() == "Ok")
+            {
+                var path = dialog.FileName;
+                var directoryInfo = new DirectoryInfo(path);
+
+                return ParseModelPaths(directoryInfo);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        private void CloseModelLinks(ModelPath modelPath)
+        {
+            // Access transmission data in the given file
+            TransmissionData trans = TransmissionData.ReadTransmissionData(modelPath);
+            if (trans != null)
+            {
+                // collect all external references
+                var externalReferences = trans.GetAllExternalFileReferenceIds();
+                foreach (ElementId extRefId in externalReferences)
+                {
+                    var extRef = trans.GetLastSavedReferenceData(extRefId);
+                    if (extRef.ExternalFileReferenceType == ExternalFileReferenceType.RevitLink)
+                    {
+//                        var p = ModelPathUtils.ConvertModelPathToUserVisiblePath(extRef.GetPath());
+                        //set data
+                        trans.SetDesiredReferenceData(extRefId, extRef.GetPath(), extRef.PathType, false);
+                        Debug.Write($"{extRef.GetPath().CentralServerPath} " +
+                                    $"{extRef.GetPath().ServerPath}");
+                    }
+
+                }
+                // make sure the IsTransmitted property is set
+                trans.IsTransmitted = true;
+
+                // modified transmissoin data must be saved back to the model
+                TransmissionData.WriteTransmissionData(modelPath, trans);
+            }
+        }
+
+        public void SyncWithCentral(Document doc)
+        {
+            // set options for accessing central model
+            var transOpts = new TransactWithCentralOptions();
+            //      var transCallBack = new SyncLockCallback();
+            // override default behavioor of waiting to try sync if central model is locked
+            //      transOpts.SetLockCallback(transCallBack);
+            // set options for sync with central
+            var syncOpts = new SynchronizeWithCentralOptions();
+            var relinquishOpts = new RelinquishOptions(true);
+            syncOpts.SetRelinquishOptions(relinquishOpts);
+            // do not autosave local model
+            syncOpts.SaveLocalAfter = false;
+            syncOpts.Comment = "Освобождено";
+            try
+            {
+                doc.SynchronizeWithCentral(transOpts, syncOpts);
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show($"Sync with model {doc.Title}", ex.Message);
+            }
+
+
+
+        }
+        private List<ModelPath> ParseModelPaths(DirectoryInfo path)
+        {
+            var revitFiles = path.GetDirectories("*.rvt", System.IO.SearchOption.AllDirectories);
+            var modelPaths = new List<ModelPath>();
+
+            // Parse filepath to RSN path
+            foreach (var filePathInfo in revitFiles)
+            {
+                var filePath = filePathInfo.FullName;
+                var rsnPath = Regex.Replace(@"RSN:\\" + filePath.Remove(0, 2), @"(?<=.ru\b).*?(?<=Prj|Projects|Prg)", "");
+                modelPaths.Add(ModelPathUtils.ConvertUserVisiblePathToModelPath(rsnPath));
+            }
+            return modelPaths;
+
+        }
+
+    }
+}
